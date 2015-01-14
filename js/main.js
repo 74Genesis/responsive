ebac4821efa6253c05eceba021cda4406d214445
74Genesis <74Genesis@gmail.com>
2015-01-14 14:36:25 +0500

    add mob menu

--- js/main.js
+++ js/main.js
@@ -7,5 +7,29 @@ $(document).ready(function() {
       else {
       	$("header").removeClass('header_scroll');
       };
+      // если моб. меню открыто, закрыть
+		if ($("nav.mobile-menu").hasClass('mob-menu-open')) {
+	   	$("nav.mobile-menu").removeClass('mob-menu-open');
+	   	$("nav.mobile-menu").animate({left:"-250px"},300);
+	   	$("nav.mobile-menu .open-but").animate({left:"0px"},300);
+		}
    });
+
+// last-child для ie
+$(".services .services__item:last-child").css("margin","0");
+
+//моб. меню
+$(".open-but").click(function() {
+	if ($("nav.mobile-menu").hasClass('mob-menu-open')) {
+   	$("nav.mobile-menu").removeClass('mob-menu-open');
+   	$("nav.mobile-menu").animate({left:"-250px"},300);
+   	$("nav.mobile-menu .open-but").animate({left:"0px"},300);
+	} else {
+   	$("nav.mobile-menu").addClass('mob-menu-open');
+   	$("nav.mobile-menu").animate({left:"0px"},300);
+   	$("nav.mobile-menu .open-but").animate({left:"250px"},300);
+	};
+});
+
+
 });
